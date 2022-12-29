975c8af6ac23e81403973a62ec681ebd7bb861dd
Sebastian Scheibe <sebastian@ecostack.nl>
2022-12-29 10:49:46 +0800

    day 10 done

--- day10/day.go
+++ day10/day.go
@@ -2,14 +2,14 @@ package day10
 
 import (
 	"AdventOfCode2022/util"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
 )
 
-func part1Fn() {
-
-}
+const CRT_X_MAX = 40
+const CRT_Y_MAX = 6
 
 func getValue(file string, part2 bool) {
 	results, err := util.GetFileContentsSplit(file)
@@ -18,24 +18,49 @@ func getValue(file string, part2 bool) {
 	cycle := 0
 	evalOutputs := []int{20, 60, 100, 140, 180, 220}
 	result := 0
+
+	crtX := 0
+	crtY := 0
+
+	printCRT := func() {
+		if crtX == x || crtX == x-1 || crtX == x+1 {
+			fmt.Print("#")
+		} else {
+			fmt.Print(".")
+		}
+	}
+
+	cycleFn := func() {
+		cycle++
+		if util.Contains(evalOutputs, cycle) {
+			result += cycle * x
+		}
+
+		printCRT()
+		crtX++
+		if crtX == CRT_X_MAX {
+			crtX = 0
+			crtY++
+			fmt.Print("\n")
+		}
+		if crtY == CRT_Y_MAX {
+			crtY = 0
+		}
+
+	}
+
 	for _, line := range results {
 		split := strings.Split(line, " ")
 		cmd := split[0]
 		if cmd == "addx" {
 			toAdd, _ := strconv.Atoi(split[1])
 			for i := 0; i < 2; i++ {
-				cycle++
-				if util.Contains(evalOutputs, cycle) {
-					result += cycle * x
-				}
+				cycleFn()
 			}
 			x += toAdd
 		}
 		if cmd == "noop" {
-			cycle++
-			if util.Contains(evalOutputs, cycle) {
-				result += cycle * x
-			}
+			cycleFn()
 		}
 	}
 	log.Println("part1", result)
